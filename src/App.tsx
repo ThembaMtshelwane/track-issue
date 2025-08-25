48a57ff16d6c6631db99bdc94da667440df71f0e
ThembaMtshelwane <thembamm3@gmail.com>
2025-08-25 23:44:18 +0200

    feat: Add filter section for status and priority in the issue tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="bg-blue-500/50 min-h-screen">
+    <div className="bg-blue-500/50 min-h-screen border max-w-[860px] mx-auto">
       <div className="p-4 flex flex-col sm:flex-row justify-between items-center">
         <div className="p-2 rounded flex items-center gap-3">
           <div className="size-12 rounded-full bg-black"></div>
@@ -13,6 +13,45 @@ function App() {
           <span>+</span>
           <span>Add Issue</span>
         </button>
+      </div>
+
+      {/* Filter section */}
+      <div className="border flex flex-col px-4 py-2 gap-4 sm:grid sm:grid-cols-[60%_40%]">
+        <div className="border border-blue-400 grid gap-2 sm:grid-cols-2">
+          <div className="grid grid-cols-2 gap-2 sm:grid-cols-1">
+            <label htmlFor="status" className="font-medium">
+              Filter by Status:
+            </label>
+            <select
+              id="status"
+              className="border border-gray-300 rounded px-2 py-1"
+            >
+              <option value="all">All</option>
+              <option value="open">Open</option>
+              <option value="in-progress">In Progress</option>
+              <option value="closed">Closed</option>
+            </select>
+          </div>
+          <div className="grid grid-cols-2 gap-2 sm:grid-cols-1">
+            <label htmlFor="priority" className="font-medium">
+              Filter by Priority:
+            </label>
+            <select
+              id="priority"
+              className="border border-gray-300 rounded px-2 py-1"
+            >
+              <option value="all">All</option>
+              <option value="low">Low</option>
+              <option value="medium">Medium</option>
+              <option value="high">High</option>
+            </select>
+          </div>
+        </div>
+        <div className="border border-red-400 items-center flex justify-center px-4 py-2 rounded">
+          <p>
+            Showing <span>{3}</span> of <span>{3}</span> issues
+          </p>
+        </div>
       </div>
     </div>
   );
