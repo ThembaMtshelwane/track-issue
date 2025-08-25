27d5476cf64478fb61b301803d711ba088b055c4
ThembaMtshelwane <thembamm3@gmail.com>
2025-08-25 23:12:59 +0200

    feat: Added the header = Name + logo+ button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="bg-blue-500/50 min-h-screen">
+      <div className="p-4 flex flex-col sm:flex-row justify-between items-center">
+        <div className="p-2 rounded flex items-center gap-3">
+          <div className="size-12 rounded-full bg-black"></div>
+          <div>
+            <h1 className="text-3xl font-bold">Issue Tracker</h1>
+            <p className="text-gray-800">Manage and track project issues</p>
+          </div>
+        </div>
+        <button className="bg-black text-white px-6 py-2 rounded flex items-center gap-2 hover:bg-gray-800 cursor-pointer hover:scale-105 transition duration-300">
+          <span>+</span>
+          <span>Add Issue</span>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
